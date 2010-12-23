246bc539f48e0a1dacfeb6395d711b214434bae2
Charles Ma <chuckjma@gmail.com>
2010-12-23 02:44:36 -0800

    re-architeched message parsing between background page and popup. code is muuch cleaner now Added a dequeue context menu item

--- js/contextMenu.js
+++ js/contextMenu.js
@@ -5,37 +5,72 @@
   console.log("tab: " + JSON.stringify(tab));
 }
 */
-var readQueue = [];
 
-function genericOnClick(info, tab) {
+var readQ = (function() {
+   //private
+   var q = [];
+   var save = function() {
+      localStorage['readqueue'] = JSON.stringify(q);
+   }
+   var load = function() {
+      if (localStorage['readqueue']) {
+         q = JSON.parse(localStorage['readqueue']);
+      }
+   }
+   return ({ //public
+      "enQ":function(item) {
+         q.push(item);
+         save();
+      },
+      "deQ":function() {
+         var item = q.shift();
+         save();
+         return item;
+      },
+      "remove":function(index) {
+         var item = q.splice(index, 1);
+         save();
+         return item;
+      },
+      "insert":function(index, item) {
+         q.splice(index, 0, item);
+         save();
+      },
+      "get":function(index) {
+         return q[index];
+      },
+      "size":function() {
+         return q.length;
+      }
+   });
+
+   // main
+   load();
+})();
+
+// config
+var context = ["link", "page", "image", "video", "audio"];
+
+// click handlers
+var genericOnClick = function(info, tab) {
    var title = tab.title;
    var url = tab.url;
    if (info['linkUrl']) {
       url = info['linkUrl'];
       title = url;
    }
-   readQueue.push({'title':title, 'url':url});
-   localStorage['readqueue'] = JSON.stringify(readQueue);
+   readQ.enQ({'title':title, 'url':url});
+}
+var deQClick = function(info, tab) {
+   var item = readQ.deQ();
+   chrome.tabs.create({'url':item.url});
 }
 
-if (localStorage['readqueue']) {
-   readQueue = JSON.parse(localStorage['readqueue']);
-}
-
-var parentItem = chrome.contextMenus.create({"title": "Add To ReadQueue", 
-      "contexts":["link", "page", "image", "video", "audio"], 
+// contextMenus
+var parentItem = chrome.contextMenus.create({"title": "Add To ReadQueue",
+      "contexts":context,
       "onclick":genericOnClick});
 
-chrome.extension.onRequest.addListener(
-   function(request, sender, sendResponse) {
-      if (request['cmd'] == "remove_item") {
-         var index = request['item'];
-         readQueue.splice(index, 1);
-         localStorage['readqueue'] = JSON.stringify(readQueue);
-         sendResponse({'response_msg': readQueue});
-      } else if (request['cmd'] == "get_readqueue") {
-         sendResponse({'response_msg': readQueue});
-      } else {
-         sendResponse({response_msg: "fail"}); // snub them.
-      }
-   });
+var deqItem = chrome.contextMenus.create({"title": "Next Q'd Page",
+      "contexts":context,
+      "onclick": deQClick});
--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,30 @@
 function $(id) {
    return document.getElementById(id);
 }
-var readQueue = [];
 
-var removeItem = function(i) {
-   chrome.extension.sendRequest({"cmd":"remove_item", "item":i});
-}
-chrome.extension.sendRequest({"cmd": "get_readqueue"}, function(response) {
-      readQueue = response['response_msg'];
+window.onload = function() {
+var bgPage = chrome.extension.getBackgroundPage();
+var readQ = bgPage.readQ;
+   for (var i = 0; i < readQ.size(); i++) {
+      var pel = document.createElement('p');
+      var el = document.createElement('a');
+      var item = readQ.get(i);
 
-      console.log(readQueue);
-      for (var i = 0; i < readQueue.length; i++) {
-         var pel = document.createElement('p');
-         var el = document.createElement('a');
-         el.setAttribute('href', readQueue[i]['url']);
-         el.innerHTML = readQueue[i]['title'];
-         el.setAttribute('q_id', i);
+      el.setAttribute('href', item['url']);
+      el.innerHTML = item['title'];
+      el.setAttribute('q_id', i);
 
-         el.onclick = function() {
-         removeItem(this.getAttribute('q_id'));
-            chrome.tabs.create({'url':this.href});
-         }
-         el.onmouseover = function() {
-            $('urlbar').innerHTML = this.href;
-         }
-         el.onmouseout = function() {
-            $('urlbar').innerHTML = "";
-         }
-         pel.appendChild(el);
-         $('links').appendChild(pel);
+      el.onclick = function() {
+         readQ.remove(i);
+         chrome.tabs.create({'url':this.href});
       }
-   });
+      el.onmouseover = function() {
+         $('urlbar').innerHTML = this.href;
+      }
+      el.onmouseout = function() {
+         $('urlbar').innerHTML = "";
+      }
+      pel.appendChild(el);
+      $('links').appendChild(pel);
+   }
+}
