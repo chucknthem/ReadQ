f8446fe93ac49d8daf9b2fc7d0b6456f82be2453
Charles Ma <chuckjma@gmail.com>
2010-12-25 14:18:15 -0800

    styles change

--- js/displayMessage.js
+++ js/displayMessage.js
@@ -1,16 +1,19 @@
 var div = document.createElement('div');
-div.backgroundColor = '#000000';
+div.style.backgroundColor = '#CCFFCC';
 div.style.display = 'block';
 div.style.position = 'fixed';
-div.style.top = '40%';
-div.style.left = '30%';
-div.style.width = '20em';
-div.style.height = '20em';
 div.style.zIndex = '100';
+div.style.top = '40%';
+div.style.marginLeft = 'auto';
+div.style.marginRight = 'auto';
+div.style.width = '100em';
+div.style.height = 'auto';
 
 chrome.extension.sendRequest({req: 'message'},
    function(response) {
+      var len = response.res.length;
       div.innerHTML = response.res;
+
       document.body.appendChild(div);
       setTimeout('document.body.removeChild(div)', 5000);
    });
