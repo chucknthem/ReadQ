fb4d869aefdce88fabdc221b545e972e820711e2
Charles Ma <chuckjma@gmail.com>
2010-12-26 07:35:15 -0800

    encapsulate display message

--- js/displayMessage.js
+++ js/displayMessage.js
@@ -1,20 +1,24 @@
-var div = document.createElement('div');
-div.style.backgroundColor = '#CCFFCC';
-div.style.display = 'block';
-div.style.position = 'fixed';
-div.style.zIndex = '100';
-div.style.top = '40%';
-div.style.marginLeft = 'auto';
-div.style.marginRight = 'auto';
-div.style.width = '100em';
-div.style.height = 'auto';
+(function() {
+   var div = document.createElement('div');
+   div.style.backgroundColor = '#CCFFCC';
+   div.style.display = 'block';
+   div.style.position = 'fixed';
+   div.style.zIndex = '100';
+   div.style.top = '40%';
+   div.style.marginLeft = 'auto';
+   div.style.marginRight = 'auto';
+   div.style.width = '100em';
+   div.style.height = 'auto';
 
-chrome.extension.sendRequest({req: 'message'},
-   function(response) {
-      var len = response.res.length;
-      div.innerHTML = response.res;
+   chrome.extension.sendRequest({req: 'message'},
+      function(response) {
+         var len = response.res.length;
+         div.innerHTML = response.res;
+         var removeDiv = function() {
+            document.body.removeChild(div);
+         }
+         document.body.appendChild(div);
+         setTimeout(removeDiv, 5000);
+      });
 
-      document.body.appendChild(div);
-      setTimeout('document.body.removeChild(div)', 5000);
-   });
-
+})();
